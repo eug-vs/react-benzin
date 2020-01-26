a526d3c85a40834257c5b78d6f31a983e42c5b97
Eug-VS <eug-vs@keemail.me>
2020-01-26 16:20:17 +0300

    feat: prepare for publish

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,5 @@
-import Window from './components/Window/Window';
-import Header from './components/Header/Header';
-import ContentSection from './components/ContentSection/ContentSection';
-import SmartList from './components/SmartList/SmartList';
-
-
-export default { Window, Header, ContentSection, SmartList };
+export { default as Window } from './components/Window/Window';
+export { default as Header } from './components/Header/Header';
+export { default as ContentSection } from './components/ContentSection/ContentSection';
+export { default as SmartList } from './components/SmartList/SmartList';
+export { default as theme } from './theme';
