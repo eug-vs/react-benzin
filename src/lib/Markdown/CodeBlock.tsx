62a543258a111f0f45be74b6ec3d887e15f67376
eug-vs <eug-vs@keemail.me>
2020-04-05 14:18:45 +0300

    feat: beautify CodeBlock

--- src/lib/Markdown/CodeBlock.tsx
+++ src/lib/Markdown/CodeBlock.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { ParserPropTypes } from './types';
+import { Paper } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    background: theme.palette.background.default,
+    padding: theme.spacing(2),
+    overflowX: 'auto',
+  },
+}));
 
 const CodeBlock: React.FC<ParserPropTypes> = ({ rawLines }) => {
+  const classes = useStyles();
   return (
-    <p style={{background: 'rgba(255, 255, 255, .1)', padding: '8px' }}>
+    <Paper variant="outlined" className={classes.root}>
       {rawLines.map(line => <> {line} <br/> </>)}
-    </p>
+    </Paper>
   );
 }
 
