46f410699037824408daf7c035458ba7bd958d44
eug-vs <eug-vs@keemail.me>
2020-04-07 20:32:10 +0300

    feat: include content before first header

--- src/lib/Markdown/Section.tsx
+++ src/lib/Markdown/Section.tsx
@@ -30,7 +30,12 @@ const Section: React.FC<PropTypes> = ({ rawLines, level = 0 }) => {
   const deeperLevelIndex = rawLines.findIndex(line => line.match(`^#{${level + 1},} .*$`));
   const rawContent = rawLines.splice(0, (deeperLevelIndex < 0) ? rawLines.length : deeperLevelIndex);
 
-  if (!level) return <ChildrenSections rawLines={rawLines} level={getHeaderLevel(rawLines[0])}/>;
+  if (!level) return (
+    <>
+      <Content rawLines={rawContent} />
+      <ChildrenSections rawLines={rawLines} level={getHeaderLevel(rawLines[0])}/>
+    </>
+  )
 
   const sectionName = rawContent.splice(0, 1)[0].slice(level).trim();
   const deeperLevel = getHeaderLevel(rawLines[0]);
